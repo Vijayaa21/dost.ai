cab929d6ddbf75df297db746cb205ec38881fc4a
Vijaya Mishra <134155725+Vijayaa21@users.noreply.github.com>
2026-02-01 01:15:55 +0530

    Refactor database settings to use dj_database_url

--- backend/dost/settings.py
+++ backend/dost/settings.py
@@ -6,6 +6,8 @@ import os
 from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
+import dj_database_url
+
 
 load_dotenv()
 
@@ -84,14 +86,11 @@ CHANNEL_LAYERS = {
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 # Custom user model
